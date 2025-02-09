5e0a718042e1b11434f128a2a1ef41674f03490b
jooya38 <hyj0102809@gmail.com>
2025-02-09 12:16:26 +0900

    IDcheck API 추가 & res 형식 수정

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,13 +8,21 @@ const signup = async (req, res) => {
         const db = req.app.get('db');
 
         if (!id || !password) {
-            res.status(400).send({ "code" : "Invalid ID or password." });
+            res.status(400).send({ 
+                "code" : 400,
+                "success" : false,
+                "message" : "Invalid ID or password."
+            });
             return;
         }
 
         const exist_user = await db.collection('user').findOne({id});
         if (exist_user) {
-            res.status(400).send({ "code" : "User already exists." });
+            res.status(409).send({
+                "code" : 409,  
+                "success" : false,
+                "message" : "User already exists."
+            });
             return;
         }
 
@@ -24,15 +32,20 @@ const signup = async (req, res) => {
             id : id,
             password : hashpw,
         })
-        // res.redirect('/')
         res.send({
+            "code": 200,
+            "success": true,
             "token": `Bearer ${tokenService.getToken(id)}`,
             "id": id
         });
 
     } catch (err) {
         console.error(err);
-        res.status(500).send({ "code" : "Server error." });
+        res.status(500).send({ 
+            "code": 500,
+            "success": false,
+            "message": "Server error."
+        });
     }
 };
 
@@ -43,18 +56,66 @@ const signin = async (req, res) => {
 
         const user = await db.collection('user').findOne({id});
         if (!user || !(await bcrypt.compare(password, user.password))) {
-            return res.status(400).send({ "code": "WRONG_USERNAME_OR_PASSWORD" });
+            return res.status(400).send({ 
+                "code": 400,
+                "success": false,
+                "message": "WRONG_USERNAME_OR_PASSWORD" 
+            });
         }
 
-        res.json({ 
+        res.send({ 
+            "code": 200,
+            "success": true,
             "token": `Bearer ${tokenService.getToken(id)}`,
             "id": id
         });
     } catch (err) {
         console.error(err);
-        res.status(500).send({ "code" : "Server error." });
+        res.status(500).send({
+            "code": 500, 
+            "success": false,
+            "message": "Server error."
+        });
+    }
+};
+
+const idcheck = async(req, res) => {
+    try{
+        const { id } = req.body;
+        const db = req.app.get('db');
+
+        if (!id) {
+            res.status(400).send({ 
+                "code": 400,
+                "id-check": false,
+                "message": "Invalid ID."
+            });
+            return;
+        }
+
+        const exist_user = await db.collection('user').findOne({id});
+        if (exist_user) {
+            res.status(409).send({ 
+                "code": 409,
+                "id-check": false,
+                "message": "User already exists."
+            });
+            return;
+        }
+        res.send({
+            "code": 200,
+            "id-check": true,
+            "message": "Availabel ID."
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ 
+            "code": 500,
+            "success": false,
+            "message": "Server error."
+        });
     }
 };
 
 
-module.exports = { signup, signin };
+module.exports = { signup, signin, idcheck };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 //API 라우터들(회원가입, 로그인 API 라우트 관련)
 const express = require('express');
-const { signup, signin } = require('../controllers/auth.controller.js');
+const { signup, signin, idcheck } = require('../controllers/auth.controller.js');
 
 const router = express.Router();
 
 router.post("/signup", signup); //회원가입
+router.post("/signup/check", idcheck); //회원가입 아이디 체크크
 router.post("/signin", signin);  //로그인
 
 module.exports = router;
